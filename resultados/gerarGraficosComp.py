23588e67f87a552780045d9b5718791496b644bb
levysantiago <levyssantiago@gmail.com>
2018-05-12 17:45:41 -0300

    Corrigindo dualidade de códigos e gráficos

--- resultados/gerarGraficosComp.py
+++ resultados/gerarGraficosComp.py
@@ -4,17 +4,23 @@ from matplotlib import gridspec
 
 def main():
 	ordemMatriz = []
+
+	tempoSequencial = []
+	tempoParalelo = []
+	desvioPadraoSequencial = []
+	desvioPadraoParalelo = []
 	speedup = []
 	eficiencia = []
 
 	speedup.append([])
 	eficiencia.append([])
 
+	flag4thread = 0
 	cont = 1
 	i = 0
 	ticks = [0]
 
-	file = open("resultadoFinalComp.out", "r")
+	file = open("resultadoFinal.out", "r")
 	# \n
 	a = file.readline()
 	while(a != ''):
@@ -22,6 +28,7 @@ def main():
 		if(a == "*----------*\n"):
 			speedup.append([])
 			eficiencia.append([])
+			flag4thread += 1
 
 			#Incrementando contador da matriz
 			i += 1
@@ -43,19 +50,27 @@ def main():
 
 			#Obtendo tempo sequencial
 			a = file.readline()
-			#tempoSequencial.append( float(a.split('*')[1]) )
+			#Pegando somente os resultados com 4 threads
+			if(flag4thread == 1):
+				tempoSequencial.append( float(a.split('*')[1]) )
 
 			#Obtendo tempo paralelo
 			a = file.readline()
-			#tempoParalelo.append( float(a.split('*')[1]) )
+			#Pegando somente os resultados com 4 threads
+			if(flag4thread == 1):
+				tempoParalelo.append( float(a.split('*')[1]) )
 
 			#Obtendo desvio padrão sequencial
 			a = file.readline()
-			#desvioPadraoSequencial.append( float(a.split('*')[1]) )
+			#Pegando somente os resultados com 4 threads
+			if(flag4thread == 1):
+				desvioPadraoSequencial.append( float(a.split('*')[1]) )
 
 			#Obtendo desvio padrão paralelo
 			a = file.readline()
-			#desvioPadraoParalelo.append( float(a.split('*')[1]) )
+			#Pegando somente os resultados com 4 threads
+			if(flag4thread == 1):
+				desvioPadraoParalelo.append( float(a.split('*')[1]) )
 
 			# \n
 			a = file.readline()
@@ -88,7 +103,37 @@ def main():
 	#print(speedup)
 	#print(eficiencia)
 
-	#GRÁFICO DE SPEEDUP
+	#GRÁFICO DE TEMPO SEQUENCIAL X TEMPO PARALELO
+	gs = gridspec.GridSpec(2, 1, height_ratios=[10, 1]) 
+	plt.subplot(gs[0])
+	plt.title("Tempo Sequencial X Tempo Paralelo")
+	plt.grid(True)
+	plt.ylabel("Tempo (seg)")
+	plt.xlabel("Ordem da Matriz")
+	plt.xticks(ticks, ordemMatriz, fontsize=8, rotation=90)
+	linha_sequencial, = plt.plot(tempoSequencial, label="Sequencial")
+	linha_paralelo, = plt.plot(tempoParalelo, label="Paralelo")
+	plt.legend(handles=[linha_sequencial, linha_paralelo])
+	
+	plt.savefig("graficos/tempo.png")
+	plt.show()
+
+	#GRÁFICO DE DESVIO PADRÃO SEQUENCIAL X DESVIO PADRÃO PARALELO
+	gs = gridspec.GridSpec(2, 1, height_ratios=[10, 1]) 
+	plt.subplot(gs[0])
+	plt.title("Desvio Padrão Sequencial X Desvio Padrão Paralelo")
+	plt.grid(True)
+	plt.ylabel("Desvio Padrão")
+	plt.xlabel("Ordem da Matriz")
+	plt.xticks(ticks, ordemMatriz, fontsize=8, rotation=90)
+	linha_sequencial, = plt.plot(desvioPadraoSequencial, label="Sequencial")
+	linha_paralelo, = plt.plot(desvioPadraoParalelo, label="Paralelo")
+	plt.legend(handles=[linha_sequencial, linha_paralelo])
+	
+	plt.savefig("graficos/desvioPadrao.png")
+	plt.show()
+
+	#GRÁFICO DE SPEEDUP COMPARANDO 2, 4 E 8 THREADS
 	gs = gridspec.GridSpec(2, 1, height_ratios=[10, 1]) 
 	plt.subplot(gs[0])
 	plt.title("Gráfico de SpeedUP")
@@ -105,7 +150,7 @@ def main():
 	plt.savefig("graficos/speedup.png")
 	plt.show()
 
-	#GRÁFICO DE EFICIÊNCIA
+	#GRÁFICO DE EFICIÊNCIA COMPARANDO 2, 4 E 8 THREADS
 	gs = gridspec.GridSpec(2, 1, height_ratios=[10, 1]) 
 	plt.subplot(gs[0])
 	plt.title("Gráfico de SpeedUP")
